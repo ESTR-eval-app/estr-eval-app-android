f12639b0d81c1036baa1761996cbe9fefa03975d
Steve <stevenlyall@gmail.com>
2016-02-09 15:29:45 -0800

    tablet app shows web app in web view, checks network status

--- app/src/main/java/ca/stevenlyall/estrevalapp/MainActivity.java
+++ app/src/main/java/ca/stevenlyall/estrevalapp/MainActivity.java
@@ -1,51 +1,74 @@
 package ca.stevenlyall.estrevalapp;
 
+import android.app.AlertDialog;
+import android.content.Context;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
+import android.provider.Settings;
 import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.Toolbar;
-import android.view.View;
-import android.view.Menu;
-import android.view.MenuItem;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
 
 public class MainActivity extends AppCompatActivity {
+
+	WebView webView;
+
+	// url for web app
+	final String URL = "http://stevenlyall.me/evaluate";
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
-		Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-		setSupportActionBar(toolbar);
+	}
 
-		FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-		fab.setOnClickListener(new View.OnClickListener() {
+	@Override
+	protected void onResume() {
+		super.onResume();
+
+		if (isNetworkConnected()) {
+			showAppInWebView();
+		} else {
+			showNoConnectionMessage();
+		}
+	}
+
+	private boolean isNetworkConnected() {
+		ConnectivityManager connectivityManager = (ConnectivityManager) getSystemService(Context.CONNECTIVITY_SERVICE);
+		NetworkInfo info = connectivityManager.getActiveNetworkInfo();
+		return (info != null && info.isConnected());
+	}
+
+	private void showNoConnectionMessage() {
+		AlertDialog.Builder builder = new AlertDialog.Builder(MainActivity.this);
+		builder.setPositiveButton("Ok", new DialogInterface.OnClickListener() {
 			@Override
-			public void onClick(View view) {
-				Snackbar.make(view, "Hello world", Snackbar.LENGTH_LONG).setAction("Action", null).show();
+			public void onClick(DialogInterface dialog, int which) {
+				// go to wifi settings
+				MainActivity.this.startActivity(new Intent(Settings.ACTION_WIFI_SETTINGS));
 			}
 		});
+		builder.setTitle(R.string.no_connection).setMessage(R.string.no_connection_detail);
+
+		builder.create().show();
 	}
 
-	@Override
-	public boolean onCreateOptionsMenu(Menu menu) {
-		// Inflate the menu; this adds items to the action bar if it is present.
-		getMenuInflater().inflate(R.menu.menu_main, menu);
-		return true;
+	private void showAppInWebView() {
+		webView = (WebView) findViewById(R.id.webView);
+
+		// enable js for web view
+		WebSettings webSettings = webView.getSettings();
+		webSettings.setJavaScriptEnabled(true);
+		webSettings.setUserAgentString("eval_tablet_app");
+
+		webView.setWebViewClient(new WebViewClient());
+		webView.loadUrl(URL);
+
 	}
 
-	@Override
-	public boolean onOptionsItemSelected(MenuItem item) {
-		// Handle action bar item clicks here. The action bar will
-		// automatically handle clicks on the Home/Up button, so long
-		// as you specify a parent activity in AndroidManifest.xml.
-		int id = item.getItemId();
 
-		//noinspection SimplifiableIfStatement
-		if (id == R.id.action_settings) {
-			return true;
-		}
-
-		return super.onOptionsItemSelected(item);
-	}
 }
